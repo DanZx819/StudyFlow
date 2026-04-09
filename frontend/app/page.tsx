e9fe21d93be7cf9fbc8d7f17b2d2d7470787ed58
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-04-09 14:08:39 -0300

    Mostrando dados

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,30 +5,28 @@ import styles from "./styles.module.css";
 import Link from "next/link";
 import api from "@/services/api";
 import { User } from "@/types/User";
+import getUser from "@/services/userService";
 
 export default function Home() {
-  const [user, setUser] = useState<User | null>(null)
+  const [user, setUser] = useState<User | null>(null);
 
-  async function getUser(){
-    try{
-      const response = await api.get<User>("/user");
-      setUser(response.data);
-    }catch(error){
-      console.error("Erro ao buscar usuário", error);
-    }
-  }
-  
-  
-  useEffect(() =>{
+  useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
-    getUser();
+    async function setandoUser() {
+      try {
+        const response = await getUser();
+        if (response) {
+          setUser(response);
+        }
+      } catch (error: unknown) {
+        console.error(error);
+      }
+    }
+    setandoUser();
   }, []);
-
-
 
   return (
     <div className={styles.container}>
-      
       {/* Saudação */}
       <section className={styles.hero}>
         <h1>Olá, {user?.name}👋</h1>
@@ -38,7 +36,6 @@ export default function Home() {
 
       {/* Cards */}
       <section className={styles.cards}>
-        
         <div className={styles.card}>
           <h2>📅 Minha Rotina</h2>
           <p>Organize seus horários de estudo</p>
@@ -46,9 +43,9 @@ export default function Home() {
         </div>
 
         <div className={styles.card}>
-          <h2>💻 Linguagens</h2>
+          <h2>💻 Matérias</h2>
           <p>Acompanhe o que você está estudando</p>
-          <Link href="/linguagens">Ver linguagens</Link>
+          <Link href="/materias">Ver matérias</Link>
         </div>
 
         <div className={styles.card}>
@@ -56,9 +53,7 @@ export default function Home() {
           <p>Veja sua evolução ao longo do tempo</p>
           <Link href="/progresso">Ver progresso</Link>
         </div>
-
       </section>
-
     </div>
   );
 }

ec086c55792f221f73b763f23c0670e68cadafcf
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-04-09 14:09:00 -0300

    Service de matérias

---
+++ frontend/services/subjectService.ts
@@ -0,0 +1,23 @@
+import api from "./api";
+
+export interface Subject{
+  id: number;
+  title: string;
+  imageUrl?: string;
+}
+
+export async function getSubjects(){
+    const response = await api.get("/subjects");
+    return response.data;
+}
+
+export async function createSubject(data: FormData) {
+    const response = await api.post<Subject>("/subjects", data, {
+        headers:{
+            "Content-Type": "multpart/form-data",
+        },
+    });
+
+    return response.data;
+}
+

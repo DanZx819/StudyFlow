37280de16f5c3c029f2e87cc87e96efafe97d464
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-04-09 14:10:07 -0300

    Corrigindo Header

--- frontend/components/header/HeaderComponent.tsx
+++ frontend/components/header/HeaderComponent.tsx
@@ -6,20 +6,41 @@ const inter = Inter({
 });
 
 import { navLinks } from "./navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 import ButtonThemeToggle from "../theme/ButtonThemeToggle";
 import { Menu, X } from "lucide-react";
+import { User } from "@/types/User";
+import getUser from "@/services/userService";
 
 
 
 
 export default function HeaderComponent() {
   
-
-
+  const [user, setUser] = useState<User | null>(null);
   const [open, setOpen] = useState(false);
+  useEffect(() =>{
+    async function setandoUser(){
+      try{
+        const response = await getUser();
+        if(response){
+          setUser(response);
+        }
+      }catch(error: unknown){
+        console.error(error)
+      }
+
+    }
+
+    setandoUser();
+  }, [open])
+
+
+
+
+
 
   function handleOpen(){
     setOpen((prev) =>{
@@ -66,8 +87,16 @@ export default function HeaderComponent() {
 
         <nav className={styles.menuList}>
           <div className={styles.profile}>
-            <h1>Foto</h1>
-            <h2>Nome</h2>
+            {user ? (
+              <>
+                <h1>{user?.name}</h1>
+                <h2>{user?.email}</h2>
+                <a href="/logout">Logout</a>
+              </>
+            ) : (
+              <p>Faça <span><a href="/login">Login</a></span> ou <span><a href="/register">Registro</a></span></p>
+            )}
+            
           </div>
           <hr />
           {navLinks.map((link) =>(

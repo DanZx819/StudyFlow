970853b55bc0860f53bb645ccfefd7b4be55354c
Daniel de Oliveira Zanchetta <147755543+DanZx819@users.noreply.github.com>
2026-04-09 14:08:27 -0300

    Rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,25 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::middleware("auth:sanctum")->group(function(){
+    //Rotas de Matérias 
+
+    Route::get("/subjects", [SubjectController::class, 'index']);
+    Route::post("/subjects", [SubjectController::class, 'store']);
+
+    //Logout
+
+    Route::post("/logout", [AuthController::class, 'logout']);
+});
 
 
 Route::post('/register', [AuthController::class, 'register']);
